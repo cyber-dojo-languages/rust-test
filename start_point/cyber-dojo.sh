74431fbdac0bb6274c04be6b045b25084733e7a1
Jon Jagger <jon@jaggersoft.com>
2020-02-12 12:00:50 +0000

    Turn warnings into errors; run clippy to produce report/style.txt

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -3,4 +3,8 @@ set -e
 PATH=/usr/local/cargo/bin:${PATH}
 #RUST_BACKTRACE=1
 
-cargo test
+rm -rf report || true
+mkdir report 2> /dev/null || true
+cargo test --features "strict" -- --nocapture
+cargo clippy 2>> ./report/style.txt
+cargo fmt
--- start_point/src/lib.rs
+++ start_point/src/lib.rs
@@ -1,3 +1,4 @@
+#![cfg_attr(feature = "strict", deny(warnings))]
 
 pub fn answer() -> i32 {
     6 * 9
